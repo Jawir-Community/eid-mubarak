919f3cdb43aea712a4a175c9d2952166cbcd28e8
Anuraaaa <anwarfauzan75@gmail.com>
2024-04-09 15:51:11 +0700

    add page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Particles from "react-particles"
+import {loadConfettiPreset} from 'tsparticles-preset-confetti'
+import { Typewriter } from "react-simple-typewriter"
+import { useState } from "react"
+import Countdown from "react-countdown"
+import music1 from './assets/music1.mp3'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState(["🎉 Menuju 1 Syawal 1445 H ✨", "Selamat tinggal Ramadhan 1445 H 👋"])
+  const [click, setClick] = useState(true)
+
+  const particleInit = async(engine) => {
+    await loadConfettiPreset(engine)
+  }
+
+  const timeLeft = () => {
+    const newYear = new Date("April 9, 2024 18:00:00").getTime()
+    const nowDate = new Date().getTime()
+    const calculateTime = newYear - nowDate
+    return calculateTime
+  }
+
+  const audio1 = new Audio()
+  audio1.src = music1
+  audio1.loop = true
+
+
+  const handleClick = () => {
+    if (click) {
+      audio1.play()
+      setClick(false)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>    
+      <Particles init={particleInit} options={{ preset: "confetti" }}/>
+      <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+        <span className="text-white lg:text-4xl text-xl font-bold z-50">
+          <Typewriter words={message} loop={false} cursor/>
+        </span>
+        <div className="z-50 text-white">
+          <Countdown date={Date.now() + timeLeft()} onComplete={() => {
+            setMessage(["Selamat Hari Raya Idul Fitri 🙏🏻", "Mohon Maaf Lahir dan Batin 🙏🏻"])
+            const button = document.getElementById('button')
+            button.classList.remove("hidden")
+          }}/>
+        </div>
+        <button className="bg-white text-black z-50 p-4 rounded-lg animate-bounce mt-8 hidden" onClick={handleClick} id="button">Click Me</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </>    
   )
 }
 
